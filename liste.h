5ffb79c245940f67a74bf5a2bd8dd02ff6d45032
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-04 09:19:19 +0100

    Add header file liste.h for linked list operations

---
+++ liste.h
@@ -0,0 +1,10 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "structures.h"
+
+Chainon* creationChainon(Station* s);
+Chainon* insertDebut(Chainon* tete, Station* s);
+Station* createStation(char* id);
+
+#endif
