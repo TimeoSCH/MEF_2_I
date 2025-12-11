3298c8d8bba489d724735d3c83a5f7864481571d
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-11 09:37:07 +0100

    Update avl.c

--- avl.c
+++ avl.c
@@ -1,1 +1,14 @@
 #include "avl.h"
+
+Arbre* creerArbre(Station* s) {
+    Arbre* nouv = (Arbre*)malloc(sizeof(Arbre));
+    if (nouv == NULL) {
+        printf("Erreur d'allocation.\n");
+        exit(1);
+    }
+    nouv->elmt = s;
+    nouv->fg = NULL;
+    nouv->fd = NULL;
+    nouv->equilibre = 0;
+    return nouv;
+}
