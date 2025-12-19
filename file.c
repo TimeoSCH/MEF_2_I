0db36cbaa9912c83ea931728168524826b0d40d0
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-19 16:37:08 +0100

    Update file.c

--- file.c
+++ file.c
@@ -19,38 +19,49 @@ void charger(char* chemin, pStation* racine, char* mode) {
     }
 
     char ligne[MAX_LIGNE];
-    // On suppose que le fichier d'entrée n'a pas d'en-tête ou qu'il est déjà filtré par le shell
-    // Si le shell ne supprime pas l'en-tête, décommentez la ligne suivante :
-    // fgets(ligne, MAX_LIGNE, fp); 
+    if (!fgets(ligne, MAX_LIGNE, fp)) {
+        fclose(fp);
+        return;
+    }
 
     while (fgets(ligne, MAX_LIGNE, fp)) {
-        // Nettoyage fin de ligne
         char *p = ligne;
         while (*p) { if (*p == '\r' || *p == '\n') *p = '\0'; p++; }
 
-        // Parsing manuel simple (strtok est aussi possible mais gardons votre logique)
-        char *cols[4] = {NULL, NULL, NULL, NULL}; // ID, CAP, CONSO
-        int i = 0;
-        char *token = strtok(ligne, ";");
-        
-        while(token != NULL && i < 3) {
-            cols[i++] = token;
-            token = strtok(NULL, ";");
+        char *cols[12]; 
+        int col_count = 0;
+        char *ptr = ligne;
+
+        // Découpage manuel
+        while (ptr != NULL && col_count < 12) {
+            char *next = strchr(ptr, ';');
+            if (next) *next = '\0';
+            cols[col_count++] = ptr;
+            ptr = next ? (next + 1) : NULL;
         }
 
-        // Mapping des colonnes selon le Shell
-        // Le shell envoie : ID;CAP;CONSO
-        // cols[0] = ID (String)
-        // cols[1] = CAP (Nombre ou 0)
-        // cols[2] = CONSO (Nombre ou 0)
+        if (estEgal(mode, "real")) {
+            // VERIFICATION : La colonne 3 (LV) doit exister et ne pas être '-'
+            if (col_count > 5 && !estEgal(cols[3], "-") && strlen(cols[3]) > 0) {
+                int id = atoi(cols[3]);
+                long load = atol(cols[5]);
+                if (load > 0) {
+                    *racine = inserer(*racine, id, cols[3], 0, load);
+                }
+            }
+        } 
+        else if (estEgal(mode, "max") || estEgal(mode, "hva") || estEgal(mode, "hvb")) {
+            char* id_str = "-";
+            if (col_count > 2 && !estEgal(cols[2], "-")) id_str = cols[2];
+            else if (col_count > 1 && !estEgal(cols[1], "-")) id_str = cols[1];
 
-        if (cols[0] && cols[1] && cols[2]) {
-            char* id_str = cols[0];
-            long cap = atol(cols[1]); // atol pour long
-            long conso = atol(cols[2]);
-
-            // Appel corrigé à inserer
-            *racine = inserer(*racine, id_str, cap, conso);
+            if (!estEgal(id_str, "-")) {
+                int id = atoi(id_str);
+                long cap = atol(cols[4]);
+                if (cap > 0) {
+                    *racine = inserer(*racine, id, id_str, cap, 0);
+                }
+            }
         }
     }
     fclose(fp);
