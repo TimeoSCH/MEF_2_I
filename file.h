534934dc7c3ede0d68364078b1eb81d429be4e5d
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-19 14:58:52 +0100

    Update file.h

--- file.h
+++ file.h
@@ -1,10 +1,13 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <stdio.h> // Nécessaire pour FILE*
 #include "avl.h"
 
-// Charge le fichier CSV dans l'arbre AVL
+// Charge le fichier CSV dans l'arbre AVL selon le mode choisi (histo)
 void charger(char* chemin, pStation* racine, char* mode);
 
+// Calcule les fuites pour une station précise (leaks)
+void traiter_fuites(char* chemin, char* station_id, FILE* f_out);
+
 #endif
- 
