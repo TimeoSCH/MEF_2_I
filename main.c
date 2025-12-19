344fd795f56ce9d5d569394049352e14aa75fafc
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-19 15:07:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,50 +3,37 @@
 #include <string.h>
 #include "file.h"
 #include "avl.h"
+#include "leaks.h" I
 
-// Fonction utilitaire pour comparer des chaînes
-int estEgal(const char* s1, const char* s2) {
+// Fonction pour comparer des chaînes
+int estEgalMain(const char* s1, const char* s2) {
     return strcmp(s1, s2) == 0;
 }
 
 int main(int argc, char** argv) {
-    if (argc < 4) {
-        return 1; 
-    }
+    if (argc < 4) return 1; 
 
     char* fichier_in = argv[1];
     char* commande = argv[2]; 
-    char* mode = argv[3]; // mode contient "max"/"src"/"real" OU l'ID pour leaks
+    char* mode = argv[3]; 
 
     pStation arbre = NULL;
 
-    // 1. Ouverture Fichier Sortie
     FILE* f_out = fopen("stats.csv", "w");
     if (f_out == NULL) return 1;
 
-    // 2. Traitement selon la commande
-    if (estEgal(commande, "histo")) {
-        // Écriture En-têtes Histo
-        if (estEgal(mode, "max")) {
-            fprintf(f_out, "identifier;max volume (k.m3.year-1)\n");
-        } 
-        else if (estEgal(mode, "src")) {
-            fprintf(f_out, "identifier;source volume (k.m3.year-1)\n");
-        } 
-        else if (estEgal(mode, "real")) {
-            fprintf(f_out, "identifier;real volume (k.m3.year-1)\n");
-        }
+    if (estEgalMain(commande, "histo")) {
+        if (estEgalMain(mode, "max")) fprintf(f_out, "identifier;max volume (k.m3.year-1)\n");
+        else if (estEgalMain(mode, "src")) fprintf(f_out, "identifier;source volume (k.m3.year-1)\n");
+        else if (estEgalMain(mode, "real")) fprintf(f_out, "identifier;real volume (k.m3.year-1)\n");
         
-        // Chargement et Traitement via AVL
         charger(fichier_in, &arbre, mode);
         infixe(arbre, f_out);
         liberer(arbre);
     }
-    else if (estEgal(commande, "leaks")) {
-        // Écriture En-tête Leaks
+    else if (estEgalMain(commande, "leaks")) {
         fprintf(f_out, "identifier;Leak volume (M.m3.year-1)\n");
         
-        // Appel de la fonction linéaire (pas d'AVL ici)
         traiter_fuites(fichier_in, mode, f_out);
     }
 
