7e23f95f55fb8bd1ebcef4500ad54057b05b1d3f
leysenshady64-sketch <leysenshady64@gmail.com>
2025-12-19 16:26:54 +0100

    Update file.c

--- file.c
+++ file.c
@@ -19,63 +19,38 @@ void charger(char* chemin, pStation* racine, char* mode) {
     }
 
     char ligne[MAX_LIGNE];
-    // Sauter l'entête
-    if (!fgets(ligne, MAX_LIGNE, fp)) {
-        fclose(fp);
-        return;
-    }
+    // On suppose que le fichier d'entrée n'a pas d'en-tête ou qu'il est déjà filtré par le shell
+    // Si le shell ne supprime pas l'en-tête, décommentez la ligne suivante :
+    // fgets(ligne, MAX_LIGNE, fp); 
 
     while (fgets(ligne, MAX_LIGNE, fp)) {
         // Nettoyage fin de ligne
         char *p = ligne;
         while (*p) { if (*p == '\r' || *p == '\n') *p = '\0'; p++; }
 
-        char *cols[12]; 
-        int col_count = 0;
-        char *token = ligne;
-        char *next_token;
-
-        // Parsing manuel robuste pour gérer les champs vides (;;)
-        while (token != NULL && col_count < 12) {
-            next_token = strchr(token, ';');
-            if (next_token) *next_token = '\0';
-            cols[col_count++] = token;
-            if (next_token) token = next_token + 1;
-            else token = NULL;
+        // Parsing manuel simple (strtok est aussi possible mais gardons votre logique)
+        char *cols[4] = {NULL, NULL, NULL, NULL}; // ID, CAP, CONSO
+        int i = 0;
+        char *token = strtok(ligne, ";");
+        
+        while(token != NULL && i < 3) {
+            cols[i++] = token;
+            token = strtok(NULL, ";");
         }
 
-        // --- MAPPING DES COLONNES (Indices typiques) ---
-        // Indice 0: Power Plant
-        // Indice 1: HVB Station
-        // Indice 2: HVA Station
-        // Indice 3: LV Station  <-- ID pour le mode 'real'
-        // Indice 4: Capacity
-        // Indice 5: Load
-        // Indice 6: ...
+        // Mapping des colonnes selon le Shell
+        // Le shell envoie : ID;CAP;CONSO
+        // cols[0] = ID (String)
+        // cols[1] = CAP (Nombre ou 0)
+        // cols[2] = CONSO (Nombre ou 0)
 
-        if (estEgal(mode, "real")) {
-            // On vérifie si la colonne LV (3) n'est pas vide
-            if (col_count > 5 && !estEgal(cols[3], "-") && strlen(cols[3]) > 0) {
-                int id = atoi(cols[3]);
-                long load = atol(cols[5]);
-                if (load > 0) {
-                    *racine = inserer(*racine, id, cols[3], 0, load);
-                }
-            }
-        } 
-        else if (estEgal(mode, "max") || estEgal(mode, "src")) {
-            // Pour HVA ou HVB, on regarde les colonnes 1 ou 2
-            char* id_str = "-";
-            if (!estEgal(cols[2], "-")) id_str = cols[2];
-            else if (!estEgal(cols[1], "-")) id_str = cols[1];
+        if (cols[0] && cols[1] && cols[2]) {
+            char* id_str = cols[0];
+            long cap = atol(cols[1]); // atol pour long
+            long conso = atol(cols[2]);
 
-            if (!estEgal(id_str, "-")) {
-                int id = atoi(id_str);
-                long cap = atol(cols[4]);
-                if (cap > 0) {
-                    *racine = inserer(*racine, id, id_str, cap, 0);
-                }
-            }
+            // Appel corrigé à inserer
+            *racine = inserer(*racine, id_str, cap, conso);
         }
     }
     fclose(fp);
