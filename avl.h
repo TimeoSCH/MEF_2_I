2edaa297604210caac063a151e83a523e14db62f
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-21 17:17:46 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -5,27 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Station {   //structure d'un AVL
-    char id_str[50];    
+// formules de rotation
+#ifndef max
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#endif
+
+//structure d'un avl
+typedef struct Station {        
+    char id_str[50];
     long capacite;
     long conso;
-    int h;
+    int equilibre; 
     struct Station *fg;
     struct Station *fd;
 } Station;
 
 typedef Station* pStation;
-
-// Utilitaires
-int max(int a, int b);
-int hauteur(pStation a);
-int equilibre(pStation a);
-
-// Rotations
-pStation rotationDroite(pStation y);
-pStation rotationGauche(pStation x);
-pStation doubleRotationGD(pStation a);
-pStation doubleRotationDG(pStation a);
 
 // Fonctions principales
 pStation creerStation(char* code, long cap, long conso);
