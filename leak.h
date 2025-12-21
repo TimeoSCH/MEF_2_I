99aa4d9890e29e0eae21af4ff064918f664b71c4
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-21 16:14:39 +0100

    Update leak.h

--- leak.h
+++ leak.h
@@ -3,11 +3,6 @@
 
 #include <stdio.h>
 
-
-void traiter_fuites(char* chemin, char* mode, FILE* f_out);
-
-// Fonctions utilitaires détectées dans les symboles de leak.o
-int estEgal_leaks(const char* s1, const char* s2);
-long chaineVersLong_leaks(const char* s);
+void traiter_fuites(char* chemin, char* station_id, FILE* f_out);
 
 #endif
