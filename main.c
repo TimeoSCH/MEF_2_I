c2f51366576f1a7b97baf6bd066333b3d90a27dc
TimeoSCH <timeo.schlumberger@gmail.com>
2025-12-21 16:20:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "avl.h"
-#include "file.h" // Fournit maintenant 'estEgal' et 'charger'
+#include "file.h" 
 #include "leak.h"
 
 int main(int argc, char** argv) {
@@ -18,19 +18,24 @@ int main(int argc, char** argv) {
     pStation arbre = NULL;
     FILE* f_out = stdout; 
 
-    // estEgal vient maintenant de file.h/file.c
     if (estEgal(commande, "histo")) {
-        // En-têtes CSV séparés par ':' pour correspondre au format de sortie AVL
-        if (estEgal(mode, "max"))      fprintf(f_out, "Station:Capacite:Consommation\n");
-        else if (estEgal(mode, "real")) fprintf(f_out, "Station:Capacite:Consommation\n");
-        else                           fprintf(f_out, "Station:Capacite:Consommation\n");
         
+        if (estEgal(mode, "max"){
+            fprintf(f_out, "Station;Capacite;Consommation\n");
+        }
+        else if (estEgal(mode, "real"){
+            fprintf(f_out, "Station;Capacite;Consommation\n");
+        }
+        else{
+            fprintf(f_out, "Station;Capacite;Consommation\n");
+        }
         charger(fichier_in, &arbre, mode);
         infixe(arbre, f_out);
         liberer(arbre);
     }
     else if (estEgal(commande, "leaks")) {
-        fprintf(f_out, "Station:Fuite\n");
+        
+        fprintf(f_out, "Station;Fuite\n");
         traiter_fuites(fichier_in, mode, f_out); 
     }
     else {
